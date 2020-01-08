03115d6033923f31e9b952d1d6dc1ea0fb1ee759
Morgan Wajda-Levie <morgan@Josephine's imac>
2020-01-08 10:22:38 -0500

    copied over mediaobject framework from xml2ryg, minus RequiresProperties metaclass

---
+++ turnovertools/mediaobject.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+class MediaObject(MediaObject):
+    """
+    Parent class for all media objects. Not meant to be instantiated directly.
+    """
+    __wraps_type__ = type(None)
+    __default_data__ = []
+    __requires_properties__ = []
+
+    @classmethod
+    def wrap_list(cls, data_list):
+        """
+        Wraps a list of data objects using the given MediaObject child
+        class, returning them in a new list.
+        """
+        mob_list = []
+        for d in data_list:
+            mob_list.append(cls(d))
+        return mob_list
+
+    def __init__(self, data=None, **kwargs):
+        """
+        Instantiate MediaObject with a new data object, or with
+        kwargs.
+        """
+        self.parent = None
+        if data is not None:
+            assert isinstance(data, self.wraps_type)
+            self.data = data
+        else:
+            self.data = self.wraps_type(*self.default_data)
+        for key, val in kwargs.items():
+            if key in self.__requires_properties__:
+                setattr(self, key, val)
+            else:
+                raise AttributeError('Invalid keyword parameter ' + key)
+
+    def __setattr__(self, key, value):
+        """
+        Optionally call a private _on_update method whenever
+        attributes are changed in this object.
+        """
+        self._on_update(key, value)
+        super(MediaObject, self).__setattr__(key, value)
+
+    def _on_update(self, key, value):
+        pass
+
+class Sequence(MediaObject):
+    pass
+
+class SequenceTrack(MediaObject):
+    pass
+
+class Event(MediaObject):
+    __requires_properties__ = ['clip_name']
+
+    def get_custom(self, name):
+        raise NotImplementedError()
+
+    @property
+    def posterframes(self):
+        """Returns a list of posterframes (in record), or rec_start_frame in
+        list form."""
+        if getattr(self, '_posterframes', None):
+            return self._posterfames
+        return [self.rec_start_frame]
+
+    @posterframes.setter
+    def posterframes(self, val):
+        self._posterframes = val;
+
+class SourceClip(MediaObject):
+    pass
+
+class Bin(MediaObject):
+    pass
+
+class DictWrapperMeta(type):
+    def __new__(meta, name, bases, class_dict):
+        lookup = class_dict.get('__lookup__', {})
+        for prop, target in lookup.items():
+            if prop not in class_dict:
+                class_dict[prop] = property(meta.getmapper(target),
+                                            meta.setmapper(target))
+        cls = type.__new__(meta, name, bases, class_dict)
+        return cls        
+
+    def getmapper(target):
+        def getter(self):
+            return self.data.get(target, None)
+        return getter
+
+    def setmapper(lookup):
+        def setter(self, val):
+            self.data[target] = val
+        return setter
+
+class DictWrapper(object, metaclass=DictWrapperMeta):
+    __wraps_type__ = dict
