1fe7d6b75d2771c79bbf9ec0bdd292a1d7358c2a
Morgan Wajda-Levie <morgan@Josephine's imac>
2020-01-12 14:52:34 -0500

    removes filler

--- scripts/xml2ryg.py
+++ scripts/xml2ryg.py
@@ -6,11 +6,23 @@ import sys
 
 from turnovertools import xmlobjects
 
+##
+# Event processing functions
+
+def remove_filler(e):
+    if e.reel is None:
+        return None
+    return e
+
 def events_from_xml(xmlpath):
     sequence = xmlobjects.XMLSequence.fromfile(xmlpath)[0]
     events = []
     for t in sequence:
         events.extend(t.events)
+    return events
+
+def sort_by_tc(events):
+    events.sort(key=lambda e: (e.rec_start_tc, e.parent.track_name))
     return events
 
 def main(**kwargs):
--- tests/test_xml2ryg.py
+++ tests/test_xml2ryg.py
@@ -17,6 +17,7 @@ TEST_XML_COMPLEX = os.path.join(TEST_FILES, 'R2-v29_Flattened.xml')
 class TestXml2rygInternals(unittest.TestCase):
     def setUp(self):
         self.input = TEST_XML_COMPLEX
+        self.events = xml2ryg.events_from_xml(TEST_XML_COMPLEX)
 
     def test_input_xml(self):
         events = xml2ryg.events_from_xml(TEST_XML_COMPLEX)
@@ -25,10 +26,31 @@ class TestXml2rygInternals(unittest.TestCase):
                 self.assertIsInstance(e, mediaobject.Event)
 
     def test_sort_events_by_timecode(self):
-        self.fail('Unwritten test.')
+        sorted_events = xml2ryg.sort_by_tc(self.events)
+        previous_timecode = ''
+        previous_track = ''
+        for i, e in enumerate(sorted_events):
+            this_timecode = e.rec_start_tc
+            this_track = e.parent.track_name
+            with self.subTest(i=i):
+                self.assertGreaterEqual(this_timecode, previous_timecode)
+                if this_timecode == previous_timecode:
+                    self.assertGreater(this_track, previous_track)
+            previous_timecode = this_timecode
+            previous_track = this_track
 
     def test_remove_filler(self):
-        self.fail('Unwritten test.')
+        expected_filler_indices = [1, 90, 257, 273, 276, 279, 287, 293, 
+                                   297, 299, 302, 307, 309, 311, 313, 
+                                   315, 320, 322, 323, 324]
+        for i, e in enumerate(self.events):
+            remove_filler = xml2ryg.remove_filler(e)
+            if remove_filler is None and i not in expected_filler_indices:
+                with self.subTest(i=i):
+                    self.fail('Unexpected event marked as filler.')
+            elif remove_filler is not None and i in expected_filler_indices:
+                with self.subTest(i=i):
+                    self.fail('Expected filler event not marked as filler.')
 
     def test_compare_existing(self):
         self.fail('Unwritten test.')
