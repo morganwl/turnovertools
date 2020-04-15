25e6985f59c1cf925012a0bbaa4de48efe91f2e0
Morgan Wajda-Levie <morgan.wajdalevie@gmail.com>
2020-04-15 17:58:20 -0400

    Rewrote vfxlist to work with mobs.VFXEvent objects

---
+++ tests/test_vfxlist.py
@@ -0,0 +1,47 @@
+""""""
+
+import csv
+import os
+import sys
+import tempfile
+import unittest
+
+USER_HOME = os.path.expanduser("~")
+TEST_DIR = os.path.dirname(__file__)
+MAIN_DIR = os.path.join(TEST_DIR, os.pardir)
+
+sys.path.insert(0, MAIN_DIR)
+
+from test_vfxevent import get_sample_vfxevent_data
+from turnovertools import vfxlist
+
+# look for private test files in the home user directory that are not
+# tracked by git
+PRIVATE_TEST_FILES = os.path.join(USER_HOME, 'private_test_files',
+                                  'turnovertools', 'mediaobjects')
+PRIVATE_TEST_MEDIA = os.path.join(PRIVATE_TEST_FILES, 'test_media')
+PRIVATE_TEST_AVID_VOLUME = os.path.join(PRIVATE_TEST_MEDIA,
+                                        'test_media_volume')
+
+PRIVATE_CONTROL_SAMPLES = os.path.join(PRIVATE_TEST_FILES,
+                                       'control_samples', 'mediaobjects')
+
+class TestVFXList(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_read_vfx_csv_vfxevents(self):
+        """Receives an iterable string (eg file object or list object)
+        containing a csv formatted event list and returns a list of only VFX
+        items as mobs."""
+        sample_data = [get_sample_vfxevent_data()]
+        with tempfile.TemporaryFile('w+t', newline='') as filehandle:
+            writer = csv.DictWriter(filehandle, fieldnames=sample_data[0])
+            writer.writeheader()
+            for vfx in sample_data:
+                writer.writerow(vfx)
+            filehandle.seek(0)
+            vfxitems = vfxlist.read_vfx_csv(filehandle)
+        for vfxevent, vfx_sample in zip(vfxitems, sample_data):
+            for attribute, val in vfx_sample.items():
+                self.assertEqual(str(getattr(vfxevent, attribute)), str(val))
--- turnovertools/vfxlist.py
+++ turnovertools/vfxlist.py
@@ -1,26 +1,31 @@
-import csv
-import os
-import sys
+"""Functions for reading vfx lists."""
 
-import edl
+import csv
 from timecode import Timecode
 
-def read_csv(inputfile):
-    rows = list()
-    with open(inputfile, newline='') as fh:
-        reader = csv.DictReader(fh)
-        for row in reader:
-            rows.append(row)
-    return rows
+from turnovertools import Config
+from turnovertools import mediaobjects as mobs
 
-def read_vfx_csv(inputfile):
+def read_csv(filehandle):
+    """Reads a csv from a file-like object and yields a dict for each
+    row."""
+    reader = csv.DictReader(filehandle)
+    for row in reader:
+        yield row
+
+def read_vfx_csv(filehandle):
+    """Parses a csv from a file-like object and returns a list of
+    mobs.VFXEvent objects."""
     vfxlist = list()
-    for clip in read_csv(inputfile):
+    for clip in read_csv(filehandle):
         if clip['vfx_id']:
-            vfxlist.append(clip)
+            remove_empty = {key: value for key, value in clip.items() if value}
+            vfxlist.append(mobs.VFXEvent(**remove_empty))
     return vfxlist
 
 def add_handles(subclip, handles=None):
+    """Adds Config.DEFAULT_HANDLES number of handles to src_tc of a
+    vfx dict."""
     if handles is None:
         handles = Config.DEFAULT_HANDLES
     start_tc = Timecode(24, subclip['src_start_tc']) - handles
@@ -30,6 +35,8 @@ def add_handles(subclip, handles=None):
     subclip['frame_count_start'] = '1001'
 
 def vfx_id_element(clip):
+    """Returns the vfx_id with an element suffix, if present, from a
+    vfx dict."""
     s = clip['vfx_id']
     if clip['vfx_element']:
         s += '_' + clip['vfx_element']
