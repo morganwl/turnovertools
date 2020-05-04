86551c1eadfed030f5d82c68bb1cffb2f30704a4
Morgan Wajda-Levie <morgan.wajdalevie@gmail.com>
2020-05-04 15:49:53 -0400

    some organizing of test structure

---
+++ tests/__init__.py
@@ -0,0 +1,13 @@
+"""Import test individual test suites."""
+
+from . import shared_test_setup
+from . import common
+from . import test_ryglist
+from . import test_vfxlist
+from . import test_csv
+from . import test_edl2csv
+from . import test_interface
+from . import test_vfxpull
+from . import test_watermark
+from . import test_xml
+from .test_mobs import test_vfxevent, test_event, test_mob, test_timecode
