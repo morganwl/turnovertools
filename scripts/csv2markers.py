bf01c3e226fe091fbfff9f9bdb6ef9d46e504880
Morgan GOH Avid <morganwl.edit@gmail.com>
2021-02-22 08:48:39 -0500

    added CSV (from FMP) to marker functionality

---
+++ scripts/csv2markers.py
@@ -0,0 +1,19 @@
+import csv
+import sys
+
+def main(marker_file):
+    markers = list()
+    with open(marker_file, newline='') as fh:
+        reader = csv.reader(fh)
+        last_marker = ('', '')
+        for row in reader:
+            marker = (row[0], row[1], f'V{row[2]}', row[3], f'VFX={row[4]}={row[5]}={row[6]}', '1')
+            if last_marker != (row[1], row[2]):
+                markers.append('\t'.join(marker))
+            last_marker = (row[1], row[2])
+    with open(marker_file, 'w') as fh:
+        for marker in markers:
+            fh.write(marker + '\n')
+
+if __name__ == '__main__':
+    main(sys.argv[1])
