00a82dcaeee119e11e376b661dbd7d18799ae6ce
Morgan Wajda-Levie (laptop) <morgan.wajdalevie@gmail.com>
2021-06-23 22:35:06 -0400

    created quick hack for creating Source records from stock mediafiles in FileMaker

---
+++ quick/stock2csv.py
@@ -0,0 +1,44 @@
+"""
+Reads a directory and creates a csv of all entries with extension '.mov'
+or '.mp4', containing the source filename, the absolute path,
+src_start_tc and src_end_tc.
+
+This csv is intended to be used for importing stock into a VFX database.
+"""
+
+import csv
+import os
+import sys
+
+from turnovertools.mediaobjects import MediaFile, Timecode
+
+ROWS = ('src_file', 'src_path', 'src_start_tc', 'src_end_tc',
+        'source_type', 'src_framerate')
+
+def get_videos(dir):
+    files = os.scandir(dir)
+    for file in files:
+        if file.name.endswith('.mov') or file.name.endswith('.mp4'):
+            yield (file.name, file.path)
+    files.close()
+
+def video_to_row(filename, filepath):
+    clip = MediaFile.probe(filepath)
+    clip.src_framerate = Timecode.normalize_framerate(clip.src_framerate)
+    return(filename, os.path.abspath(filepath), clip.src_start_tc,
+            clip.src_end_tc, 'Stock', clip.src_framerate)
+    
+def main(inputdir):
+    if inputdir.endswith('/'):
+        inputdir = inputdir[:-1]
+    output = os.path.join(inputdir,
+                         os.path.basename(inputdir) + '.csv')
+    with open(output, 'wt', newline='') as fh:
+        writer = csv.writer(fh)
+        writer.writerow(ROWS)
+        for filename, filepath in get_videos(inputdir):
+            writer.writerow(video_to_row(filename, filepath))
+    print(output)
+
+if __name__ == '__main__':
+    main(sys.argv[1])
